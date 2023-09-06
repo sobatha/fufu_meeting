3e2256aabd268f810a4b644bfc7114cea84056cb
mochi47 <111059ryu@gmail.com>
2023-09-06 23:15:42 +0900

    WIP: Integrate bulma ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<div id='contents' class='container' >
+  <nav class='is-flex is-flex-direction-column'>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+  </nav>
+
+  <slot></slot>
+</div>
+
+<style lang='scss'>
+</style>
+
+<script>
+  import 'bulma/css/bulma.css'
+  import 'bulma/sass/utilities/initial-variables.sass'
+</script>
