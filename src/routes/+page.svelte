81a741fc15ccb60807ffde2d454c41bfd06d80c2
sobatha <natsuki.yokoizo.ny@gmail.com>
2023-09-16 22:43:54 +0900

    feature: 音声録音まで

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,72 @@
+<script lang='ts'>
+	let isRecording = false;
+	let chunks: Blob[] = [];
+	let mediaRecorder = main();
+
+	async function main() {
+		if (typeof window === 'undefined') return;
+		const player = document.querySelector('#player');
+		URL.revokeObjectURL(player.src)
+
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: false,
+			audio: true
+		});
+
+		if (!MediaRecorder.isTypeSupported('audio/webm')) {
+			console.warn('audio/webm is not supported');
+		}
+
+		const mediaRecorder = new MediaRecorder(stream, {
+			mimeType: 'audio/webm'
+		});
+
+		mediaRecorder.addEventListener('dataavailable', (event) => {
+			// player.src = URL.createObjectURL(event.data);
+			chunks.push(event.data);
+		});
+
+		mediaRecorder.addEventListener('stop', () => {
+			const blob = new Blob(chunks, { type: 'audio/webm' });
+			URL.revokeObjectURL(player.src)
+			player.src = URL.createObjectURL(blob);
+			// Clear the chunks for next recording
+			chunks = [];
+			// URL.revokeObjectURL()
+		});
+		
+		return mediaRecorder
+	}
+
+	function onClickStart (recorder: any) {
+		// if(!recorder) return;
+		recorder.start();
+		isRecording = true;
+		// return null
+	};
+
+	const onClickStop = (recorder) => {
+		if(!recorder) return
+		recorder.stop();
+		isRecording = false;
+		// return null
+	};
+
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
+{#await mediaRecorder then recorder}
 <div>
-    <button type="button" id="buttonStart" >Start</button>
-    <button type="button" id="buttonStop">Stop</button>
+	{ recorder }
+	<button type="button" id="buttonStart" on:click={()=>onClickStart(recorder)} disabled={isRecording}>Start</button>
+	<button type="button" id="buttonStop" on:click={()=>onClickStop(recorder)} disabled={!isRecording}>Stop</button>
 </div>
+{/await}
 <div>
-  <audio controls id="player"></audio>
+	<audio controls id="player" />
 </div>
 
 <style>
 </style>
-
-<script>
-
-async function main () {
-    if (typeof window === 'undefined') return
-    console.log('222222')
-  const buttonStart = document.querySelector('#buttonStart')
-  const buttonStop = document.querySelector('#buttonStop')
-  const player = document.querySelector('#player')
-
-  const stream = await navigator.mediaDevices.getUserMedia({ // <1>
-    video: false,
-    audio: true,
-  })
-
-  if (!MediaRecorder.isTypeSupported('audio/webm')) { // <2>
-    console.warn('audio/webm is not supported')
-  }
-
-  const mediaRecorder = new MediaRecorder(stream, { // <3>
-    mimeType: 'audio/webm',
-  })
-
-  buttonStart.addEventListener('click', () => {
-    mediaRecorder.start() // <4>
-    buttonStart.setAttribute('disabled', '')
-    buttonStop.removeAttribute('disabled')
-  })
-
-  buttonStop.addEventListener('click', () => {
-    mediaRecorder.stop() // <5>
-    buttonStart.removeAttribute('disabled')
-    buttonStop.setAttribute('disabled', '')
-  })
-
-  mediaRecorder.addEventListener('dataavailable', event => { // <6>
-    player.src = URL.createObjectURL(event.data)
-  })
-}
-
-main()
-</script>
